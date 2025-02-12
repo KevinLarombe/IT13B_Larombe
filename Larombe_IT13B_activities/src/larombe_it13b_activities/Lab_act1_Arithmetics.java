26b64382876746b386bfe43530e1815370d47a5c
joeymiels <20211595@nbsc.edu.ph>
2025-02-12 10:15:16 +0800

    Added Helloworld class

---
+++ Larombe_IT13B_activities/src/larombe_it13b_activities/Lab_act1_Arithmetics.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package larombe_it13b_activities;
+
+/**
+ *
+ * @author User
+ */
+public class Lab_act1_Arithmetics {
+    public static void main (String args []){
+                
+    System.out.println("HelloWorld");
+    
+    }
+}

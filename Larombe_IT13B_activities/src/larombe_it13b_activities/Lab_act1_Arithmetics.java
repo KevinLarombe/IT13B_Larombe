f9c68051b196964aa3bdfd1f0c707623f00b75e8
krystal cuarteros <charita121279@gmail.com>
2025-02-13 12:34:46 +0800

    Added Java Code

--- Larombe_IT13B_activities/src/larombe_it13b_activities/Lab_act1_Arithmetics.java
+++ Larombe_IT13B_activities/src/larombe_it13b_activities/Lab_act1_Arithmetics.java
@@ -4,14 +4,24 @@
  */
 package larombe_it13b_activities;
 
-/**
- *
- * @author User
- */
+import java.util.Scanner;
 public class Lab_act1_Arithmetics {
     public static void main (String args []){
                 
-    System.out.println("HelloWorld");
+    int x;
+    int y;
+    int z;
+    Scanner in = new Scanner (System.in);
+    System.out.println("Enter num1: ");
+    x= in.nextInt();
+    System.out.println("Enter num2: ");
+    y= in.nextInt();
+    System.out.println("Enter num3: ");
+    z= in.nextInt();    
+    System.out.println(x + " * " + y + " + " + z + " = " + (x * y + z));
+    System.out.println("(" + x + " - " + y + ")" + " % " + z + " = " + ((x - y) % z));
+    System.out.println("(" + x + " + " + y + " + " + z + " ) " + " / " + 3 + " = " + (( x + y + z) / 3));
+    System.out.println(x + " * " + z + " - " + "(" + y + " * " + y + ")" + " = " + (x * z - ( y * y )));
     
     }
 }

48676c64ba190fda41714cc081e003aed103d7b9
Giancyril <imyourjulie28@gmail.com>
2025-02-24 11:10:47 +0800

    Find The Greatest Number

---
+++ Larombe_IT13B_activities/Lab_Exercise 2_Find the greatest number/src/lab_exercise/pkg2_find/the/greatest/number/Lab_Exercise2_FindTheGreatestNumber.java
@@ -0,0 +1,37 @@
+
+package lab_exercise.pkg2_find.the.greatest.number;
+
+import java.util.Scanner;
+
+public class Lab_Exercise2_FindTheGreatestNumber {
+
+  
+   public static void main(String[] args){
+        Scanner in = new Scanner (System.in);
+        
+        
+        System.out.println("enter first number: ");
+        int A = in.nextInt();        
+        
+        System.out.println("enter second number: ");
+        int B = in.nextInt();
+        
+        System.out.println("enter third number: ");
+        int C = in.nextInt();
+        
+        
+        int greatest = A;
+        if (B > greatest) {
+            greatest = B;
+        }
+        if (C > greatest) {
+            greatest = C;
+        }
+        
+        
+        System.out.println("The greatest among the three numbers is: " + greatest);
+        
+        in.close();
+        
+    }
+}
